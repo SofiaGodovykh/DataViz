0e43bae5465bdab04750a7195b6e8908d20af9d7
Sofia <sgodovykh@gmail.com>
2024-07-27 16:52:35 -0700

    csv shrink

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const svg = d3.select("#container")
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("https://github.com/SofiaGodovykh/DataViz/blob/main/adult.csv", null, function (data) {
-    //d3.select("#container")
+d3.csv("https://raw.githubusercontent.com/sofiagodovykh/DataViz/master/adult.csv", function (data) {
+    
 
     // X axis: scale and draw:
     var x = d3.scaleLinear()
@@ -23,7 +23,10 @@ d3.csv("https://github.com/SofiaGodovykh/DataViz/blob/main/adult.csv", null, fun
 
     // set the parameters for the histogram
     var histogram = d3.histogram()
-        .value(function(d) { return d.education; })   // I need to give the vector of value
+        .value(function(d) {
+
+            return d.age; })
+        // I need to give the vector of value
         .domain(x.domain())  // then the domain of the graphic
         .thresholds(x.ticks(70)); // then the numbers of bins
 
@@ -32,7 +35,7 @@ d3.csv("https://github.com/SofiaGodovykh/DataViz/blob/main/adult.csv", null, fun
 
     // Y axis: scale and draw:
     var y = d3.scaleLinear()
-        .range([height, 0]);
+        .range([height, d3.max(bins, function(d) { return d.length; })]);
     y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
     svg.append("g")
         .call(d3.axisLeft(y));
@@ -43,8 +46,10 @@ d3.csv("https://github.com/SofiaGodovykh/DataViz/blob/main/adult.csv", null, fun
         .enter()
         .append("rect")
         .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("transform", function(d) {
+            console.log(d);
+            return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
         .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
         .attr("height", function(d) { return height - y(d.length); })
         .style("fill", "#69b3a2");
-});
+})
